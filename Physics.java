5be3acb31d2efd4a9f7ae05819e006aa5fbe3555
kool-kitty343 <1011mikebaker@gmail.com>
2025-03-06 21:37:41 -0500

    1

--- Physics.java
+++ Physics.java
@@ -1,16 +1,42 @@
 public class Physics
   {
+    double distance;
+    double XYangle;
+    double Zangle;
+
+    int current;
+    int other;
+
+    Particle Current = new Particle();
+    Particle Other = new Particle();
     boolean shutdown = false;
     public void Physics()
     {
       while(!shutdown)
       {
+        for (current = 1; current <= 123; current++)
+        {
+          //set current particle
 
+          for (other = 1; other <= 123; other++)
+          {
+            //set other particle
+
+            calcDistanceAndAngle();
+            calcGravity();
+            calcElectromagnetism();
+
+
+
+          }
+        }
       }
     }
     private void calcDistanceAndAngle()
     {
-
+      double DeltaX = Other.getX() - ;
+      double DeltaY = ;
+      double DeltaZ = ;
     }
     private void calcGravity()
     {
